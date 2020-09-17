5254a1f1d3858fbff7f1f5f7594146abe94fe330
machukhinktato <tarabrinmv@gmail.com>
2020-09-17 18:29:22 +0300

    done

---
+++ pyqt/11.py
@@ -0,0 +1,15 @@
+"""
+Создание приложения на Kivy
+"""
+
+# 1. Загрузка служебных пакетов
+"""
+python -m pip install --upgrade pip wheel setuptools
+python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+python -m pip install kivy.deps.gstreamer --extra-index-url https://kivy.org/downloads/packages/simple/
+"""
+
+# 2. Загрузка фреймворка
+"""
+pip install kivy
+"""
